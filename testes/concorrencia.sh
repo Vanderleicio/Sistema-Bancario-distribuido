34d7022a5cd76505778f4ba173b72dc6aaee1c12
Vanderleicio <vanderleiciojr397@gmail.com>
2024-07-08 16:11:58 -0300

    Consertando arquivos de teste

--- testes/concorrencia.sh
+++ testes/concorrencia.sh
@@ -52,35 +52,41 @@ saldos() {
 }
 
 echo "Fazendo depósitos simultâneos:"
-deposito "Banco do LARSID" "[\"12345678901\"]" "50" $BANCO_DO_LARSID &
-deposito "Banco do LARSID" "[\"12345678901\"]" "25" $BANCO_DO_LARSID &
-deposito "LARSIDesco" "[\"12345678901\"]" "39" $LARSIDESCO &
+deposito "Banco do LARSID" "[\"12345678901\"]" "3" $BANCO_DO_LARSID &
+#deposito "Banco do LARSID" "[\"12345678901\"]" "5" $BANCO_DO_LARSID &
+deposito "LARSIDesco" "[\"12345678901\"]" "5" $LARSIDESCO &
 
 #echo "Fazendo retiradas simultâneas:"
 #saque "Banco do LARSID" "[\"12345678901\"]" "7" $BANCO_DO_LARSID &
 #saque "Banco do LARSID" "[\"12345678901\"]" "16" $BANCO_DO_LARSID &
 #saque "LARSIDesco" "[\"12345678901\"]" "10" $LARSIDESCO &
+saque "LARSIDBank" "[\"23456789012\"]" "8" $LARSIDBANK &
 
 wait
 
 echo "SALDOS PÓS DEPÓSITOS: "
 saldos $BANCO_DO_LARSID "12345678901"
 
-origens_var='[{"id": 1, "banco": "Banco do LARSID", "valor": "3"}, {"id": 1, "banco": "LARSIDesco", "valor": "5"}]'
-destino_var='{\"id\": 1, \"banco\": \"LARSIDBank\", \"cpf\": [\"23456789012\"]}'
-
+origem1='[{"id": 1, "banco": "Banco do LARSID", "valor": "3"}, {"id": 1, "banco": "LARSIDesco", "valor": "5"}]'
+origem2='[{"id": 1, "banco": "Banco do LARSID", "valor": "2"}]'
+origem3='[{"id": 1, "banco": "Banco do LARSID", "valor": "10"}, {"id": 1, "banco": "LARSIDesco", "valor": "3"}]'
+origem4='[{"id": 1, "banco": "Banco do LARSID", "valor": "100"}, {"id": 1, "banco": "LARSIDesco", "valor": "5"}]'
+origem5='[{"id": 1, "banco": "Banco do LARSID", "valor": "5"}, {"id": 1, "banco": "LARSIDesco", "valor": "5"}]'
+origem6='[{"id": 1, "banco": "Banco do LARSID", "valor": "10"}, {"id": 1, "banco": "LARSIDesco", "valor": "5"}]'
+destino1='{\"id\": 1, \"banco\": \"LARSIDBank\", \"cpf\": [\"23456789012\"]}'
+destino2='{\"id\": 2, \"banco\": \"Banco do LARSID\", \"cpf\": [\"12345678901\", \"23456789012\"]}'
 
 echo "Fazendo transferências simultâneas:"
-transferencia $BANCO_DO_LARSID '[{"id": 1, "banco": "Banco do LARSID", "valor": "3"}, {"id": 1, "banco": "LARSIDesco", "valor": "5"}]' '{\"id\": 1, \"banco\": \"LARSIDBank\", \"cpf\": [\"23456789012\"]}' &
-transferencia $BANCO_DO_LARSID '[{"id": 1, "banco": "Banco do LARSID", "valor": "3"}, {"id": 1, "banco": "LARSIDesco", "valor": "5"}]' '{\"id\": 1, \"banco\": \"LARSIDBank\", \"cpf\": [\"23456789012\"]}' &
-transferencia $BANCO_DO_LARSID '[{"id": 1, "banco": "Banco do LARSID", "valor": "2"}]' '{\"id\": 2, \"banco\": \"Banco do LARSID\", \"cpf\": [\"12345678901\", \"23456789012\"]}' &
-transferencia $BANCO_DO_LARSID '[{"id": 1, "banco": "Banco do LARSID", "valor": "10"}, {"id": 1, "banco": "LARSIDesco", "valor": "3"}]' '{\"id\": 2, \"banco\": \"Banco do LARSID\", \"cpf\": [\"12345678901\", \"23456789012\"]}' &
-transferencia $BANCO_DO_LARSID '[{"id": 1, "banco": "Banco do LARSID", "valor": "100"}, {"id": 1, "banco": "LARSIDesco", "valor": "5"}]' '{\"id\": 1, \"banco\": \"LARSIDBank\", \"cpf\": [\"23456789012\"]}' &
-transferencia $BANCO_DO_LARSID '[{"id": 1, "banco": "Banco do LARSID", "valor": "5"}, {"id": 1, "banco": "LARSIDesco", "valor": "5"}]' '{\"id\": 1, \"banco\": \"LARSIDBank\", \"cpf\": [\"23456789012\"]}' &
-transferencia $BANCO_DO_LARSID '[{"id": 1, "banco": "Banco do LARSID", "valor": "10"}, {"id": 1, "banco": "LARSIDesco", "valor": "5"}]' '{\"id\": 1, \"banco\": \"LARSIDBank\", \"cpf\": [\"23456789012\"]}' &
-transferencia $BANCO_DO_LARSID '[{"id": 1, "banco": "Banco do LARSID", "valor": "5"}, {"id": 1, "banco": "LARSIDesco", "valor": "5"}]' '{\"id\": 1, \"banco\": \"LARSIDBank\", \"cpf\": [\"23456789012\"]}' &
-transferencia $BANCO_DO_LARSID '[{"id": 1, "banco": "Banco do LARSID", "valor": "10"}, {"id": 1, "banco": "LARSIDesco", "valor": "5"}]' '{\"id\": 1, \"banco\": \"LARSIDBank\", \"cpf\": [\"23456789012\"]}' &
-transferencia $BANCO_DO_LARSID '[{"id": 1, "banco": "Banco do LARSID", "valor": "5"}, {"id": 1, "banco": "LARSIDesco", "valor": "5"}]' '{\"id\": 1, \"banco\": \"LARSIDBank\", \"cpf\": [\"23456789012\"]}' &
+transferencia $BANCO_DO_LARSID "$origem1" "$destino1" & #Sai: U1B1: 3, U1B2: 5/ Entra: U2B3: 8
+transferencia $BANCO_DO_LARSID "$origem1" "$destino1" & #Sai: U1B1: 3, U1B2: 5/ Entra: U2B3: 8
+transferencia $BANCO_DO_LARSID "$origem2" "$destino2" & #Sai: U1B1: 2/ Entra: U3B1: 2
+transferencia $BANCO_DO_LARSID "$origem3" "$destino2" & #Sai: U1B1: 10, U1B2: 3/ Entra: U3B1: 13
+transferencia $BANCO_DO_LARSID "$origem4" "$destino1" & #Sai: U1B1: 100, U1B2: 5/ Entra: U2B3: 105 ERRO
+transferencia $BANCO_DO_LARSID "$origem5" "$destino1" & #Sai: U1B1: 5, U1B2: 5/ Entra: U2B3: 10
+transferencia $BANCO_DO_LARSID "$origem6" "$destino1" & #Sai: U1B1: 10, U1B2: 5/ Entra: U2B3: 15
+transferencia $BANCO_DO_LARSID "$origem5" "$destino1" & #Sai: U1B1: 5, U1B2: 5/ Entra: U2B3: 10
+transferencia $BANCO_DO_LARSID "$origem6" "$destino1" & #Sai: U1B1: 10, U1B2: 5/ Entra: U2B3: 15
+transferencia $BANCO_DO_LARSID "$origem5" "$destino1" & #Sai: U1B1: 5, U1B2: 5/ Entra: U2B3: 10
 
 wait
 
